7ce146fbdf35b4e9af4bca663fd2c88f51fa3eec
sashenme <sashenpasindu@gmail.com>
2020-03-26 16:05:31 +0530

    Change language order

--- save.php
+++ save.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "corona";
+$username = "coronavi_sashen";
+$password = "Sashen93";
+$dbname = "coronavi_corona";
 
 $language = $_POST['txt-language'];
 $gender = $_POST['txt-gender'];
